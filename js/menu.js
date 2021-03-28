264a5ea49bd23493cb8a0cf06c41b33ab4465fa0
Serhiy Korytskyy <korysergey55@gmail.com>
2021-03-28 20:52:29 +0300

    js-mobile-button-done

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,10 @@
 (() => {
-    const menuBtnRef = document.querySelector("[bata-menu-button]");
-
+    const menuBtnRef = document.querySelector("[data-menu-button]");
+    const mobileMenuRef = document.querySelector("[data-menu]");
     menuBtnRef.addEventListener("click", () => {
 
     menuBtnRef.classList.toggle("is-open");
-
+    
+    mobileMenuRef.classList.toggle("mobile-open");
     });
 })();
