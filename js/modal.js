c27049cb7315ba5c19b1dee362844999915516bf
Serhiy Korytskyy <korysergey55@gmail.com>
2021-03-29 19:48:09 +0300

    sosial-link-hover-fixed

--- js/modal.js
+++ js/modal.js
@@ -2,7 +2,7 @@
     const refs = {
       openModalBtn: document.querySelector('[data-modal-open]'),
       closeModalBtn: document.querySelector('[data-modal-close]'),
-      backdrop: document.querySelector('[data-modal]'),
+      backdrop: document.querySelector('[data-backdrop]'),
     };
   
     refs.openModalBtn.addEventListener('click', toggleModal);
